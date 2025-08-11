82e4087708d0fa3015c7377516c67f561e626353
fpoisson2 <francis.poisson2@gmail.com>
2025-08-11 09:11:45 -0400

    Switch map type via modal and style layer button

--- tests/test_equipment_page.py
+++ tests/test_equipment_page.py
@@ -160,7 +160,7 @@ def test_equipment_detail_page_loads(make_app):
     assert html.find('id="map-container"') < html.find('id="zones-table"')
 
 
-def test_equipment_page_has_layer_button(make_app):
+def test_equipment_page_has_layer_modal(make_app):
     app = make_app()
     client = app.test_client()
     login(client)
@@ -170,6 +170,8 @@ def test_equipment_page_has_layer_button(make_app):
         resp = client.get(f"/equipment/{eq.id}")
     html = resp.data.decode()
     assert "button.id = 'layer-btn'" in html
+    assert 'id="layer-modal"' in html
+    assert 'name="map-type"' in html
     assert "google.com/vt/lyrs=y" in html
     assert "google.com/vt/lyrs=m" in html
 
